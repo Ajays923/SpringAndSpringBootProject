f2db4d171563bd091a5c15724d1e3eaa89ee98c6
TL <TL@gmail.com>
2022-10-11 11:55:49 +0530

    SpringDataJPAProj10-DateAndTimeInMySQL

---
+++ SpringDataJPAProj10-DateAndTimeInMySQL/src/main/java/com/nt/SpringDataJpaProj10DateAndTimeApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaProj10DateAndTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaProj10DateAndTimeApplication.class, args);
+	}
+
+}
